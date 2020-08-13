c49ab328c41f15c6b40de919f38a54c1ede73b51
Safal Sapkota <49337062+Safal30@users.noreply.github.com>
2020-08-14 00:10:17 +1000

    master art.py

---
+++ art.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+print(F'''                                        
+ -safal30
+''')
+
+text = input("Keyword: ")
+
+url = "http://artii.herokuapp.com/make?text="
+new_url = (url + text)
+
+print(new_url)
+
+url_req = requests.get(new_url)
+url_text = url_req.text
+url_code = url_req.status_code
+
+def fetch(saf):
+    if url_code == 200:
+        url_bs = BeautifulSoup(url_text, "html.parser")
+        print(url_bs)
+    else:
+        print("Unexpected error") 
+fetch(url_text)
